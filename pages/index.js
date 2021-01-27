a4e0094b82e3a64ec197b9383314c2350a9a7f0e
amsalimsema <salimsema@yahoo.com>
2021-01-27 12:32:47 +0300

    testing img optimization with nextJs

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ const Index = () => (
   <>
     <Layout title='Home'>
       <p>Welcome to the Home page!</p>
-      <Image src='/test.png' alt='me' width='600' height='300' />
+      <Image src='/test.png' alt='me' width='3840' height='2154' />
     </Layout>
   </>
 );
